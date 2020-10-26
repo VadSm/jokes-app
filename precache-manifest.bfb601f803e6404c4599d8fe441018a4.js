42f8a72997ce6281c5ec3c1476ff280f70ea122d
VadSm <vadsmirnov2@gmail.com>
2020-10-26 13:14:00 +0300

    Updates

---
+++ precache-manifest.bfb601f803e6404c4599d8fe441018a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad6f801b55591f36e4a1266fbc49fef",
+    "url": "/jokes-app/index.html"
+  },
+  {
+    "revision": "1eadc6554d538fa0f451",
+    "url": "/jokes-app/static/css/main.fbc5afa8.chunk.css"
+  },
+  {
+    "revision": "0ad0ec706420ae73d894",
+    "url": "/jokes-app/static/js/2.60a21a3d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/jokes-app/static/js/2.60a21a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eadc6554d538fa0f451",
+    "url": "/jokes-app/static/js/main.cc11baab.chunk.js"
+  },
+  {
+    "revision": "0b6cd2faf5a94ebafda6",
+    "url": "/jokes-app/static/js/runtime-main.6c05cc91.js"
+  }
+]);
